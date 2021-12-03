12f51b1278f243eb10202642bf94de5f311253d8
Colby Thomas <coloradocolby@gmail.com>
2021-12-03 12:00:26 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -77,3 +77,14 @@ function Dashboard() {
     </div>
   );
 }
+// # [[redirects]]
+// #   from = "/why"
+// #   to = "/about"
+// #   status = 200
+// #   force = true
+
+// # [[redirects]]
+// #   from = "/colby/*"
+// #   to = "https://coloradocolby.netlify.app/:splat"
+// #   status = 200
+// #   force = true
